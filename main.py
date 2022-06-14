5eeee5eaa053735c406f5fecfb588eb49bf2b44c
allberto <albertitovalls97@gmail.com>
2022-06-14 12:11:18 +0200

    checkpoint 2

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+
 import csv
 def read_data(fichero1, fichero2):
     
     with open(fichero1, 'r') as file:
         reader = csv.reader(file)
+        diccionario={}
         for row in reader:
+            diccionario[row[0]]=row[2]
             print(row)
 
-    with open(fichero2, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
 
+    
+   
 
 if __name__ == "__main__":
     file1 = "stops_data.csv"
